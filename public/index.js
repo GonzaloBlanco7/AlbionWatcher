077a69ebefdd5a2318f68c1535dc6a2340dad4b8
GonzaloBlanco7 <46811229+GonzaloBlanco7@users.noreply.github.com>
2021-12-10 13:07:03 +0100

    Style and languages to filter/display names added

--- public/index.js
+++ public/index.js
@@ -1,6 +1,4 @@
 
-
-
 async function getItemList() {
     const ITEM_LIST_URL = "https://raw.githubusercontent.com/broderickhyman/ao-bin-dumps/master/formatted/items.json";
     
@@ -19,12 +17,6 @@ async function getItemList() {
 
         let data = await response.json()
 
-        //Filter unwanted items
-        //FIXME: check if correct
-        data = data.filter(item => {
-            return item.LocalizedNames //Only if has name
-        })
-
         return parseItemList(data);
 
     } catch (error) {
@@ -33,6 +25,12 @@ async function getItemList() {
 }
 
 function parseItemList(list) {
+    //Filter unwanted items
+    //FIXME: check if correct
+    list = list.filter(item => {
+        return item.LocalizedNames //Only if has name
+    })
+
     return list.map((item) => {
         let tier = item.UniqueName.substr(0, 2);
         if (tier[0] === 'T') {
@@ -47,62 +45,52 @@ function parseItemList(list) {
 }
 
 function filterItems(search) {
+    const language = getLanguage();
     return item_list.filter((item) => {
         try{
-            const spanish = item.LocalizedNames["ES-ES"].toLowerCase()
-            const english = item.LocalizedNames["EN-US"].toLowerCase()
-            return (spanish.includes(search) || english.includes(search))
+            const name = item.LocalizedNames[language].toLowerCase()
+            return (name.includes(search))
         } catch(error) {
             return false;
         }
     });
 }
 
-document.getElementById("search_box").addEventListener("input", (event) => {
+document.getElementById("search_form").addEventListener("submit", (event) => {
+    event.preventDefault();
     const search = document.getElementById("search_box").value.toLowerCase();
     renderItems(filterItems(search));
 })
 
 function renderItems(items) {
-    console.log("Rendering items:", items)
-    const item_list_box = document.getElementById("item_list_box");
+    console.log("Rendering items:", items);
+    const language = getLanguage();
+    const items_table_body = document.getElementById("items_table_body");
+    items_table_body.innerHTML=""; //Clear
 
-    item_list_box.innerHTML= "";
-    let table = document.createElement('table');
-    table.style.width = '100%';
-    table.style.border = '1px solid black';
-    
-    //Table header
-    const header = table.insertRow();
-    const th_id = header.insertCell();
-    th_id.innerHTML= "ID";
-    const th_tier = header.insertCell();
-    th_tier.innerHTML= "TIER";
-    const th_name_en = header.insertCell();
-    th_name_en.innerHTML="NAME";
-    const th_name_es = header.insertCell();
-    th_name_es.innerHTML= "NOMBRE";
-
-    //each item row
+    // Each item row
     items.forEach(item => {
         try {
-            const tr = table.insertRow();
+            const tr = items_table_body.insertRow();
             const td_id = tr.insertCell();
             td_id.innerHTML= item.UniqueName;
             const td_tier = tr.insertCell();
             if (item.tier != undefined ) {
                 td_tier.innerHTML= item.tier;
             }
-            const td_name_en = tr.insertCell();
-            td_name_en.innerHTML= item.LocalizedNames["EN-US"];
-            const td_name_es = tr.insertCell();
-            td_name_es.innerHTML= item.LocalizedNames["ES-ES"];
-        } catch (error) { /*pass*/ }
+            const td_name = tr.insertCell();
+            td_name.innerHTML= item.LocalizedNames[language];
+        } catch (error) { 
+            console.error("could not render item:", item);
+        }
     })
-
-    item_list_box.appendChild(table);
 }
 
+
+function getLanguage() {
+    const language = document.querySelector('input[name="language"]:checked').value;
+    return language;
+}
 
 const item_list = await getItemList()
 renderItems(item_list)
